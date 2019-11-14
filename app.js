1ae37fc70ab41c648f6facc77e19e21ce26ef403
ckpd84 <ckpd84@gmail.com>
2019-11-14 11:53:21 -0300

    requeri el archivo index y cree los cases para las url

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const http = require('http');
-const router = require('./router');
+const fs = require("fs");
+
+const data = require("./data/index");
 
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-    res.end (router)
+    // Route System
+	switch (req.url) {
+		// Home
+		case '/':
+            res.end('Home');
+            //llamar a home
+			break;
+		// En cartelera
+		case '/en-cartelera':
+			res.end('En cartelera');
+			break;
+		case '/mas-votadas':
+			res.end('Más Votadas');
+			break;
+		case '/sucursales':
+			res.end('Sucursales');
+			break;
+		case '/contacto':
+			res.end('Contacto');
+			break;
+		case '/preguntas-frecuentes':
+			res.end('Preguntas Frecuentes');
+			break;
+		default:
+			res.end('404 not found')}
     // ??
 }).listen(3030, 'localhost', () => console.log('Server running in 3030 port'));
